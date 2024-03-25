4358ad519062e2a6d2214c1b62e362f90d22208f
M4rm3ladov <renier.antonio20@gmail.com>
2024-03-26 04:14:32 +0800

    added feed page

--- resources/views/shared/left_sidebar.blade.php
+++ resources/views/shared/left_sidebar.blade.php
@@ -5,8 +5,10 @@
                 <a class="{{ Route::is('dashboard') ? 'text-white bg-primary rounded' : ''}} nav-link text-dark" href="{{ route('dashboard') }}">
                     <span>Home</span></a>
             </li>
-        </ul>
-        <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
+            <li class="nav-item">
+                <a class="{{ Route::is('feed') ? 'text-white bg-primary rounded' : ''}} nav-link text-dark" href="{{ route('feed') }}">
+                    <span>Feed</span></a>
+            </li>
             <li class="nav-item">
                 <a class="{{ Route::is('terms') ? 'text-white bg-primary rounded' : ''}} nav-link text-dark" href="{{ route('terms') }}">
                     <span>Terms</span></a>
